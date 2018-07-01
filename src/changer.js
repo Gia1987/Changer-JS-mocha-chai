eb909df3da42e42bb0e83488ce1e0630fadf4ca9
Gia1987 <giacomo.n27@gmail.com>
2018-07-01 11:37:04 +0100

    Implemented module changer to pass 5p test. test pass (Green)

--- src/changer.js
+++ src/changer.js
@@ -3,7 +3,9 @@ function Changer() {
 }
 
 Changer.prototype.giveChange = function(amount) {
-  if(amount > 1){
+  if(amount > 4){
+      return ['5p']
+    }else if (amount > 1) {
       return ['2p']
     }else{
       return ['1p']
